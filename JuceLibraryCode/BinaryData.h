7faef74c5e14d17647c55359ebc6160018a67c2c
Nik Reiman <nik.reiman@gmail.com>
2013-01-19 19:11:52 +0100

    Add new files to project

--- JuceLibraryCode/BinaryData.h
+++ JuceLibraryCode/BinaryData.h
@@ -6,8 +6,11 @@
 
 namespace BinaryData
 {
+    extern const char*   icon_png;
+    const int            icon_pngSize = 190077;
+
     extern const char*   Constants_h;
-    const int            Constants_hSize = 1099;
+    const int            Constants_hSize = 1387;
 
     // If you provide the name of one of the binary resource variables above, this function will
     // return the corresponding data and its size (or a null pointer if the name isn't found).
