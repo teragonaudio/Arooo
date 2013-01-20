e4f4d9733e2e76fc64f1ce0b628cd4c31a6b70f9
Nik Reiman <nik.reiman@gmail.com>
2013-01-20 10:07:32 +0100

    Add some more base freqs for the howl

--- Source/Constants.h
+++ Source/Constants.h
@@ -28,17 +28,13 @@ static const int kBufferSizeBase2 = 10; // 2^10
 
 static const float kSampleRate = 44100.0f;
 
-// Also check this number of buckets above and below the target bucket index.
-// So if the bucket index is 33, this would check buckets 32 & 34.
-static const int kBucketTolerance = 1;
-
 // The minimum strength of the bucket value in order to trigger a hit
 static const float kBucketMinimumStrength = 20.0f; // TODO: Needs tweaking
 
 // TODO: Would be nice to use real frequencies instead of bucket indexes
 // These must be recalibrated for different FFT buffer sizes
-static const int kHowlBucketIndexes[] = { 14, 30, 526 };
-static const int kHowlBucketNumIndexes = 3;
+static const int kHowlBucketIndexes[] = { 14, 15, 16, 30, 31, 32, 526, 527, 528 };
+static const int kHowlBucketNumIndexes = 9;
 
 // If all buckets contain at least this many hits, a howl has been found
 static const int kHowlDetectionNumPointsNeeded = 15;
