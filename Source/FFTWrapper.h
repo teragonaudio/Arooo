ce30853111a0a1791d76469a883435512093c2ff
Nik Reiman <nik.reiman@gmail.com>
2013-12-30 23:19:42 +0100

    Inline FFTWrapper class

--- Source/FFTWrapper.h
+++ Source/FFTWrapper.h
@@ -15,22 +15,36 @@
 
 #if USE_FFT_FIXED_SIZE
 #include "FFTRealFixLen.h"
+
 #else
 #include "FFTReal.h"
 #endif
 
 class FFTWrapper {
 public:
-  FFTWrapper();
-  ~FFTWrapper();
+    FFTWrapper() {
+#if USE_FFT_FIXED_SIZE
+        fftProcessor = new ffft::FFTRealFixLen<kBufferSizeBase2>;
+#else
+        fftProcessor = new ffft::FFTReal<float>(kBufferSize);
+#endif
+    }
 
-  void doFFT(const float *inputs, float *fftData);
+    virtual ~FFTWrapper() {
+        if (fftProcessor) {
+            delete fftProcessor;
+        }
+    }
+
+    void doFFT(const float *inputs, float *fftData) {
+        fftProcessor->do_fft(fftData, inputs);
+    }
 
 private:
 #if USE_FFT_FIXED_SIZE
-  ffft::FFTRealFixLen<kBufferSizeBase2> *fftProcessor;
+    ffft::FFTRealFixLen<kBufferSizeBase2> *fftProcessor;
 #else
-  ffft::FFTReal<float> *fftProcessor;
+    ffft::FFTReal<float> *fftProcessor;
 #endif
 };
 
