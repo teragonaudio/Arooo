f70249f3c1328bc64d5a9648c49435c556193d6e
Nik Reiman <nik.reiman@gmail.com>
2013-12-30 21:19:41 +0100

    New background

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -1,26 +1,24 @@
 /*
   ==============================================================================
 
-  This is an automatically generated file created by the Jucer!
-
-  Creation date:  23 Jan 2013 8:52:25pm
+  This is an automatically generated GUI class created by the Introjucer!
 
   Be careful when adding custom code to these files, as only the code within
   the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
   and re-saved.
 
-  Jucer version: 1.12
+  Created with Introjucer version: 3.1.0
 
   ------------------------------------------------------------------------------
 
-  The Jucer is part of the JUCE library - "Jules' Utility Class Extensions"
-  Copyright 2004-6 by Raw Material Software ltd.
+  The Introjucer is part of the JUCE library - "Jules' Utility Class Extensions"
+  Copyright 2004-13 by Raw Material Software Ltd.
 
   ==============================================================================
 */
 
-#ifndef __JUCER_HEADER_MAINCOMPONENT_MAINCOMPONENT_BB374BEC__
-#define __JUCER_HEADER_MAINCOMPONENT_MAINCOMPONENT_BB374BEC__
+#ifndef __JUCE_HEADER_9002020A4DD09B20__
+#define __JUCE_HEADER_9002020A4DD09B20__
 
 //[Headers]     -- You can add your own extra header files here --
 #include "JuceHeader.h"
@@ -36,8 +34,7 @@
     Describe your class and how it works here!
                                                                     //[/Comments]
 */
-class MainComponent  : public Component,
-                       public ButtonListener
+class MainComponent  : public Component
 {
 public:
     //==============================================================================
@@ -51,8 +48,10 @@ public:
 
     void paint (Graphics& g);
     void resized();
-    void buttonClicked (Button* buttonThatWasClicked);
 
+    // Binary resources:
+    static const char* background_png;
+    static const int background_pngSize;
 
 
 private:
@@ -60,17 +59,29 @@ private:
     //[/UserVariables]
 
     //==============================================================================
-    TextEditor* logOutput;
-    TextButton* analyzeFileButton;
-    Label* statusLabel;
-    Label* eventLogLabel;
+    ScopedPointer<TextEditor> logOutput;
+    ScopedPointer<teragon::IndicatorLight> eventLight;
+    ScopedPointer<teragon::ImageKnobSmall> toleranceKnob;
+    ScopedPointer<teragon::ImageKnobSmall> cooldownKnob;
+    ScopedPointer<teragon::ToggleButton> analyzeButton;
+    ScopedPointer<teragon::ToggleButton> loadSampleButton;
+    ScopedPointer<teragon::PushButton> normalizeSampleButton;
+    ScopedPointer<teragon::PushButton> playSampleButton;
+    ScopedPointer<teragon::PushButton> sendMidiButton;
+    ScopedPointer<teragon::PushButton> noteOrCcButton;
+    ScopedPointer<teragon::ImageKnobSmall> midiChannelKnob;
+    ScopedPointer<teragon::ImageKnobSmall> midiNoteKnob;
+    ScopedPointer<teragon::ImageKnobSmall> midiVelocityKnob;
+    ScopedPointer<teragon::PushButton> runScriptButton;
+    ScopedPointer<teragon::ToggleButton> editScriptButton;
+    Image cachedImage_background_png;
 
 
     //==============================================================================
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent);
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
 
 //[EndFile] You can add extra defines here...
 //[/EndFile]
 
-#endif   // __JUCER_HEADER_MAINCOMPONENT_MAINCOMPONENT_BB374BEC__
+#endif   // __JUCE_HEADER_9002020A4DD09B20__
